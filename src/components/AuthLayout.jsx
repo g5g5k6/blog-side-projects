4002cbff94584d17f5b169ad493a68cd67ca877f
g5g5k6 <a0860191@gmail.com>
2025-06-27 22:08:29 +0800

    update Protected

---
+++ src/components/AuthLayout.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+
+function Protected({children, authentication = true}) {
+    return (
+        <>{children}</>
+    )
+    
+}
+
+export default Protected
