9336bbf9137e14bea5a259657002e19494d44533
g5g5k6 <a0860191@gmail.com>
2025-06-27 22:07:04 +0800

    update router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,82 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
 import './index.css'
 import App from './App.jsx'
+import {createBrowserRouter, RouterProvider} from "react-router-dom"
+import { Provider } from 'react-redux'
+import store from "./store/store.js"
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import Home from "./pages/Home.jsx"
+import Login from "./pages/Login.jsx"
+import AuthLayout from "./components/AuthLayout.jsx"
+import Signup from "./pages/Signup.jsx"
+import AllPost from './pages/AllPost.jsx'
+import AddPost from "./pages/AddPost.jsx"
+import EditPost from "./pages/EditPost.jsx"
+import Post from "./pages/Post.jsx"
+
+const router = createBrowserRouter([{
+  path:"/",
+  element:<App />,
+  children:[
+    {
+      path:"/",
+      element:<Home />
+    },
+    {
+      path:"/login",
+      element:(
+        <AuthLayout authentication={false}>
+          <Login /> 
+        </AuthLayout>
+      )
+    },
+    {
+      path: "/signup",
+          element: (
+            <Protected authentication={false}>
+              <Signup />
+            </Protected>
+          )
+    },
+    {
+      path: "/all-post",
+      element: (
+        <Protected authentication={true}>
+          <AllPost />
+        </Protected>
+      )
+    },
+    {
+      path: "/add-post",
+      element: (
+        <Protected authentication>
+          <AddPost />
+        </Protected>
+        )
+    },
+    {
+      path: "/edit-post/:slug",
+      element: (
+        <Protected authentication>
+          <EditPost />
+        </Protected>
+      )
+    },
+    {
+      path: "/post/:slug",
+      element: (
+        <Protected authentication>
+          <Post />
+        </Protected>
+      )
+    }
+  ]
+}])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </React.StrictMode>,
 )
